5364b44907759dbded0e7d86e51b1699501716a7
SkySnkrs <skysnkrs@gmail.com>
2025-01-15 15:35:26 -0700

    added house model details

--- Models/House.cs
+++ Models/House.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace gregslist_dotnet.Models;
 
 public class House
 {
     public int Id { get; set; }
+    [MinLength(0), MaxLength(100000)] public int sqft { get; set; }
+    [MinLength(0), MaxLength(1000)] public int bedrooms { get; set; }
+    [MinLength(0), MaxLength(1000)] public int bathrooms { get; set; }
+    [Url] public string img_url { get; set; }
+    public string description { get; set; }
+    public int price { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public Account Creator { get; set; }
 }
